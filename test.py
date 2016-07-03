0358bd8792436b141c22cabfb98d1ba1c1ca38f4
sikuquanshu123 <sikuquanshu123@hotmail.com>
2016-07-03 09:26:47 +0800

    website

---
+++ test.py
@@ -0,0 +1,82 @@
+from flask import Flask, request,render_template,redirect, url_for, abort, session
+from flask.ext.admin import Admin, BaseView, expose
+app = Flask(__name__)
+
+@app.route("/generate_204")
+def generate_204():
+    return render_template("204.html")
+
+@app.route("/")
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        if username in user_list:
+            response = make_response(redirect('/home'))
+            response.set_cookie('username', value=username, max_age=300)
+            session['islogin'] = '1'
+            return response
+        else:
+            session['islogin'] = '0'
+            return redirect('/about')
+    else:
+        return render_template('login.html')
+
+    
+@app.route("/home")
+def home():
+    return render_template('/home/home.html')
+
+@app.route("/feihua")
+def feihua():
+    return render_template('/feihua/feihua.html')
+
+@app.route("/archive")
+def archive():
+    return render_template('archive.html')
+
+@app.route("/post")
+def post():
+    return render_template('post.html')
+
+@app.route("/read_more")
+def read_more():
+    return render_template('read_more.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/love")
+def love():
+    return render_template('/row/love/520.html')
+
+@app.route("/row/2016/04/27/test")
+def test():
+    return render_template('/row/2016/04/27/test.html')
+
+@app.route("/row/2016/05/21/10086")
+def android_10086():
+    return render_template('/row/2016/05/21/10086.html')
+
+@app.route("/row/2016/05/25/xiaoqing")
+def xiaoqing_60():
+    return render_template('/row/2016/05/25/⁮xiaoqing_60.html')
+
+@app.route("/row/2016/05/28/socket")
+def socket_test():
+    return render_template('/row/2016/05/28/socket.html')
+
+@app.route("/row/2016/06/22/androidFile")
+def androidFile():
+    return render_template('/row/2016/06/22/androidFile.html')
+
+@app.route("/row/2016/06/27/shixun")
+def shixun():
+    return render_template('/row/2016/06/27/shixun.html')
+
+if __name__ == "__main__":
+    app.run()
---
+++ test.wsgi
@@ -0,0 +1,8 @@
+import sys
+sys.path.insert(0, "c:/pure_web")
+from test import app
+
+#Put logging code (and imports) here ...
+
+#Initialize WSGI app object
+application = app
